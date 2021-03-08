41761c301fd9531ce393c052ead37fb0b8d4cc4b
owopqowo <54467533+owopqowo@users.noreply.github.com>
2021-03-08 16:00:10 +0900

    add loading sppiner

--- src/components/upload/upload.jsx
+++ src/components/upload/upload.jsx
@@ -1,25 +1,34 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './upload.module.css';
 
 const Upload = ({imageUploader, name, onFileChange}) => {
+  const [isLoading, setIsLoading] = useState(false);
   const inputRef = useRef();
+  
   const onButtonClick = e => {
     e.preventDefault();
     inputRef.current.click();
   }
+
   const onChange = async e => {
+    setIsLoading(true);
     const uploaded = await imageUploader.upload(e.target.files[0]);
+    setIsLoading(false);
     onFileChange({
       name: uploaded.original_filename,
       url: uploaded.url
     });
   }
+
   return (
     <div className={styles.file}>
       <input ref={inputRef} className={styles.input} type="file" accept="images/*" name="file" onChange={onChange} />
-      <button className={styles.button} onClick={onButtonClick}>
-        {name || 'No file'}
-      </button>
+      {isLoading ? 
+        <span className={styles.sppiner}></span> :
+        <button className={`${styles.button} ${name ? styles.pink : styles.grey}`} onClick={onButtonClick}>
+          {name || 'No file'}
+        </button>
+      }
     </div>
   )
 };
